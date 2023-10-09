9ae915a1aadf5c7ce38c8ef5ade1ed888afe53af
Milo CARTAL <105420069+milocartal@users.noreply.github.com>
2023-10-09 22:42:07 +0200

    Big redo

--- js/script.js
+++ js/script.js
@@ -1,5 +1,28 @@
-if (document.getElementById('my-work-link')) {
-  document.getElementById('my-work-link').addEventListener('click', () => {
-    document.getElementById('my-work-section').scrollIntoView({behavior: "smooth"})
+if (document.getElementById('work-link')) {
+  document.getElementById('work-link').addEventListener('click', () => {
+    document.getElementById('work-section').scrollIntoView({behavior: "smooth"})
   })
+}
+
+if (document.getElementById('contact-link')) {
+  document.getElementById('contact-link').addEventListener('click', () => {
+    document.getElementById('contact').scrollIntoView({behavior: "smooth"})
+  })
+}
+
+if (document.getElementById('cv-link')) {
+  document.getElementById('cv-link').addEventListener('click', () => {
+    document.getElementById('cv-section').scrollIntoView({behavior: "smooth"})
+  })
+}
+
+if(document.getElementById("tel")){
+  document.getElementById("tel").addEventListener('click',()=>{
+    navigator.clipboard.writeText("+33649572160")
+  });
+}
+
+async function copyTel(){
+  await navigator.clipboard.writeText("+33649572160")
+  alert("Numero de téléphone copié")
 }
