47f8753798c0c45e99dd4a8bb806a25820b0f9f7
Milo CARTAL <105420069+milocartal@users.noreply.github.com>
2024-01-23 00:22:26 +0100

    redo all

--- js/script.js
+++ js/script.js
@@ -1,28 +1,29 @@
-if (document.getElementById('work-link')) {
-  document.getElementById('work-link').addEventListener('click', () => {
-    document.getElementById('work-section').scrollIntoView({behavior: "smooth"})
-  })
-}
 
-if (document.getElementById('contact-link')) {
-  document.getElementById('contact-link').addEventListener('click', () => {
-    document.getElementById('contact').scrollIntoView({behavior: "smooth"})
-  })
-}
+if (document.getElementById("tel")) {
+  document.getElementById("tel").addEventListener('click', () => {
+    navigator.clipboard.writeText("+33649572160");
+    showToast();
 
-if (document.getElementById('cv-link')) {
-  document.getElementById('cv-link').addEventListener('click', () => {
-    document.getElementById('cv-section').scrollIntoView({behavior: "smooth"})
-  })
-}
-
-if(document.getElementById("tel")){
-  document.getElementById("tel").addEventListener('click',()=>{
-    navigator.clipboard.writeText("+33649572160")
   });
 }
 
-async function copyTel(){
+async function copyTel() {
   await navigator.clipboard.writeText("+33649572160")
   alert("Numero de téléphone copié")
+}
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
+
+function showToast() {
+  var x = document.getElementById("snackbar");
+  x.className = "show";
+  setTimeout(function () { x.className = x.className.replace("show", ""); }, 3000);
 }
